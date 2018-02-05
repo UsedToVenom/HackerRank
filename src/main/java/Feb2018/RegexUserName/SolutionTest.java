524bb4cbb6a94c9e79a34a65d96289b935471883
CADUCEUS\Venom <marekj.szulc@gmail.com>
2018-02-05 14:12:28 +0100

    Day-1 Venom & Kivi co-coaching

---
+++ src/main/java/Feb2018/RegexUserName/SolutionTest.java
@@ -0,0 +1,16 @@
+package Feb2018.RegexUserName;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
---
+++ src/main/java/Feb2018/RegexUserName/UsernameValidator.java
@@ -0,0 +1,5 @@
+package Feb2018.RegexUserName;
+
+public class UsernameValidator {
+    public static final String regularExpression = "[a-zA-Z]{1}(([a-zA-Z]|[0-9]|\\_){7,29})";
+}
